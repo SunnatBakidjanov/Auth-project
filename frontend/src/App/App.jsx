4261dfff7aa638700f16408ac0a042b1af39bfdb
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-05-27 09:44:29 +0500

    Инициализация проекта
    --Добавлены линтеры
    --Организована начальная структура приложения

---
+++ frontend/src/App/App.jsx
@@ -0,0 +1,6 @@
+import './reset.scss';
+import './main.scss';
+
+export const App = () => {
+	return <div>Hello World</div>;
+};
---
+++ frontend/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App/App';
+
+const rootElement = document.getElementById('root');
+const reactRoot = createRoot(rootElement);
+
+reactRoot.render(<App />);
